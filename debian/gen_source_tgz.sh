3c4814729d61c4f9c146b3d9dbcd952563216bb3
Thiago Marcos P. Santos <thiago.santos@intel.com>
2014-05-02 12:18:44 +0300

    Debianized

---
+++ debian/gen_source_tgz.sh
@@ -0,0 +1,5 @@
+VERSION="1.0"
+DISTRO="saucy"
+PACKAGE_VERSION="2"
+
+tar -czf pycrosswalk_$VERSION~$DISTRO$PACKAGE_VERSION.orig.tar.gz --exclude-vcs --exclude=debian --exclude=out --transform="s:^pycrosswalk:pycrosswalk-$VERSION~$DISTRO$PACKAGE_VERSION:S" pycrosswalk/
