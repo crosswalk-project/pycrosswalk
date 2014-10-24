7ad380970b66bb7726dd9ecf532d46f91b73e10d
Thiago Marcos P. Santos <thiago.santos@intel.com>
2014-10-25 01:05:49 +0300

    Bump version follow-up

--- debian/gen_source_tgz.sh
+++ debian/gen_source_tgz.sh
@@ -1,5 +1,5 @@
-VERSION="1.1"
-DISTRO="saucy"
+VERSION="1.2"
+DISTRO="trusty"
 PACKAGE_VERSION="1"
 
 tar -czf pycrosswalk_$VERSION~$DISTRO$PACKAGE_VERSION.orig.tar.gz --exclude-vcs --exclude=debian --exclude=out --transform="s:^pycrosswalk:pycrosswalk-$VERSION~$DISTRO$PACKAGE_VERSION:S" pycrosswalk/
